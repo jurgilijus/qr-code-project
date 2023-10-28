42f26d8dd7951ac60ec6974760560aacc1caed63
Jurgis Subačius <38658881+jurgilijus@users.noreply.github.com>
2023-10-28 14:45:11 +0300

    page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import qr from "../../task/images/image-qr-code.png";
+
 function App() {
   return (
     <>
-      <div>
-        <h1>Hello</h1>
+      <div className="qr-table">
+        <img className="qr-img" src={qr} alt="qr code" />
+        <div className="text-conteiner">
+          <h2>Improve your front-end skills by building projects</h2>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the net level
+          </p>
+        </div>
       </div>
     </>
   );
